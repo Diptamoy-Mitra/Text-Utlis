4711ebc0e7708ab25884ddf4a8d6c77280313e73
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-06-22 17:55:46 +0530

    change  mode

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import TextForm from './components/TextForm';
 
 function App() {
   const [mode, setMode] = useState('light')  //wheater dark mode enable or not
-  const toggleMode=()=>{
-    if(mode==='light'){
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
       setMode('dark');
-      document.body.style.backgroundColor='grey'
+      document.body.style.backgroundColor = '#042743';
+      //showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
-      document.body.style.backgroundColor='white'
+      document.body.style.backgroundColor = 'white';
+      //showAlert("Light mode has been enabled", "success");
     }
   }
 
@@ -25,7 +28,7 @@ function App() {
       <Navbar title="Text Utlis" aboutText="Website is under constraction,wait for more features...." mode={mode} toggleMode={toggleMode}></Navbar>
       {/* <Navbar ></Navbar> */}
       <div className="container my-5">
-         <TextForm heading="Enter The Text To Analyze Below"/>
+         <TextForm heading="Enter The Text To Analyze Below" mode={mode}/>
          {/* <About></About> */}
       </div>
      
